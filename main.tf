ac0c600f908b00e5d32e621d1806d640260b7fac
mohanaswathi1666 <95328717+mohanaswathi1666@users.noreply.github.com>
2025-10-31 15:28:05 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,48 @@
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
-resource "aws_instance" "example" {
-ami           = "ami-02d26659fd82cf299" # Example Ubuntu AMI for ap-south-1
-instance_type = "t3.micro"
-tags = {
-   Name = "Terraform-EC2"
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Spacelift-VPC"
+  }
 }
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+  tags = {
+    Name = "Spacelift-public-subnet"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "Spacelift-internet-gateway"
+  }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Terraform-route-table"
+  }
+}
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_s3_bucket" "terraform_bucket" {
+  bucket        = "138251-mohanaswathi-demo-bucket-assignment3"
+  force_destroy = true
+  tags = {
+    Name        = "Spacelift-S3-bucket"
+    Environment = "Dev"
+  }
 }
