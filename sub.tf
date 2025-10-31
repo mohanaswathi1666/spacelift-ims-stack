3fe60a47af0d837317e8ad5ac3dcfe1746acc831
mohanaswathi1666 <95328717+mohanaswathi1666@users.noreply.github.com>
2025-10-31 19:29:29 +0530

    Update sub.tf

--- sub.tf
+++ sub.tf
@@ -3,51 +3,61 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.1.0/16"
 }
+
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
   default     = "Terraform-VPC"
 }
+
 variable "subnet_cidr" {
   description = "CIDR block for the public subnet"
   type        = string
   default     = "10.0.6.0/24"
 }
+
 variable "subnet_name" {
   description = "Name of the public subnet"
   type        = string
   default     = "Terraform-public-subnet"
 }
+
 variable "availability_zone" {
   description = "Availability zone for the subnet"
   type        = string
   default     = "ap-south-1a"
 }
+
 variable "igw_name" {
   description = "Internet Gateway name"
   type        = string
   default     = "Terraform-internet-gateway"
 }
+
 variable "route_table_name" {
   description = "Route table name"
   type        = string
   default     = "Terraform-route-table"
 }
+
 variable "bucket_name" {
   description = "S3 bucket name (must be globally unique)"
   type        = string
-  default     = "spacelift-assignment-3"
+  default     = "spacelift-assignment-3-swathi-20251031"
 }
+
 variable "bucket_tag_name" {
   description = "Name tag for the S3 bucket"
   type        = string
   default     = "Terraform-S3-bucket"
 }
+
 variable "environment" {
   description = "Environment tag for the S3 bucket"
   type        = string
