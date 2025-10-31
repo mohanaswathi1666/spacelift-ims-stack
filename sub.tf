5acfb14197b8565b43aedd287957f4e63e91ceb4
mohanaswathi1666 <95328717+mohanaswathi1666@users.noreply.github.com>
2025-10-31 19:38:24 +0530

    Update sub.tf

--- sub.tf
+++ sub.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.1.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
