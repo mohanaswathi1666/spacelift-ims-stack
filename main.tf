867707f4e80a3a611547d7b31051a95d286585da
mohanaswathi1666 <95328717+mohanaswathi1666@users.noreply.github.com>
2025-10-31 15:36:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,48 +1,54 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_vpc" "main_vpc" {
+
+resource "aws_vpc" "swathi_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "Spacelift-VPC"
+    Name = "Swathi-VPC"
   }
 }
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
+
+resource "aws_subnet" "swathi_public_subnet" {
+  vpc_id                  = aws_vpc.swathi_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
   tags = {
-    Name = "Spacelift-public-subnet"
+    Name = "Swathi-public-subnet"
   }
 }
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main_vpc.id
+
+resource "aws_internet_gateway" "swathi_gw" {
+  vpc_id = aws_vpc.swathi_vpc.id
   tags = {
-    Name = "Spacelift-internet-gateway"
+    Name = "Swathi-internet-gateway"
   }
 }
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
+
+resource "aws_route_table" "swathi_public_rt" {
+  vpc_id = aws_vpc.swathi_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.swathi_gw.id
   }
   tags = {
-    Name = "Terraform-route-table"
+    Name = "Swathi-route-table"
   }
 }
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+
+resource "aws_route_table_association" "swathi_public_assoc" {
+  subnet_id      = aws_subnet.swathi_public_subnet.id
+  route_table_id = aws_route_table.swathi_public_rt.id
 }
-resource "aws_s3_bucket" "terraform_bucket" {
-  bucket        = "138251-mohanaswathi-demo-bucket-assignment3"
+
+resource "aws_s3_bucket" "swathi_terraform_bucket" {
+  bucket        = "swathi-demo-bucket-assignment3"
   force_destroy = true
   tags = {
-    Name        = "Spacelift-S3-bucket"
+    Name        = "Swathi-S3-bucket"
     Environment = "Dev"
   }
 }
