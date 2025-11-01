71d04f420bcacc2a9b7a017c60cf760243142f5f
mohanaswathi1666 <95328717+mohanaswathi1666@users.noreply.github.com>
2025-11-01 12:13:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "aws_vpc" "existing_vpc" {
 
 resource "aws_subnet" "example_subnet" {
   vpc_id            = data.aws_vpc.existing_vpc.id
-  cidr_block        = "10.0.1.0/24"  # Updated to a valid range within 10.0.0.0/16
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 
   tags = {
@@ -21,5 +21,57 @@ resource "aws_internet_gateway" "gw" {
 
   tags = {
     Name = "datasource-Terraform-internet-gateway"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  description = "Allow RDS access"
+  vpc_id      = data.aws_vpc.existing_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # You can restrict this to your IP range
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.example_subnet.id]
+
+  tags = {
+    Name = "RDS Subnet Group"
+  }
+}
+
+resource "aws_db_instance" "example_rds" {
+  identifier              = "example-rds"
+  allocated_storage       = 20
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  name                    = "exampledb"
+  username                = "admin"
+  password                = "123456789" # Use secrets manager in production
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  skip_final_snapshot     = true
+  publicly_accessible     = true
+
+  tags = {
+    Name = "Example-RDS"
   }
 }
